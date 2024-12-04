b600723fd73751892b70b0f601313b47da334d72
nathanbuyr <G00421082@atu.ie>
2024-12-04 00:58:09 +0000

    Home.js populated with cards

--- project/src/components/Home.js
+++ project/src/components/Home.js
@@ -1,8 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
 
-{/* Header Function*/ }
-{/* Using seprated components,it separates concerns and makes the app modular*/ }
-const Home = () => {
-    return <h1>hi</h1>;
-  };
-  
-  export default Home;
+var Home = () => {
+  var [pokemonList, setPokemonList] = useState([]);
+
+  useEffect(() => {
+    var fetchPokemon = async () => {
+        var response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=50'); // Fetching the api data
+        var data = await response.json(); // Converting to JSON
+
+        var pokemonWithDetails = await Promise.all( // Promise makes sure every pokemon is fetched
+          data.results.map(async (pokemon) => 
+            {
+            var pokemonDetails = await fetch(pokemon.url);
+            return await pokemonDetails.json();
+          })
+        );
+        setPokemonList(pokemonWithDetails);
+
+    };
+
+    fetchPokemon();
+  }, []);
+
+  // Creating the container and cards for each pokemon
+  return (
+    <Container className="d-flex flex-wrap justify-content-center">
+      <h1 className="text-center w-100 my-4">Pokémon List</h1>
+      {pokemonList.map((pokemon) => 
+      (
+        <Card
+          key={pokemon.id}
+          style={{ width: '150px', margin: '10px' }}
+        >
+          <Card.Img
+            variant="top"
+            src={pokemon.sprites.front_default}
+            alt={pokemon.name}
+          />
+          <Card.Body>
+            <Card.Title className="text-center">{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</Card.Title>
+          </Card.Body>
+        </Card>
+      ))}
+    </Container>
+  );
+};
+
+export default Home;
